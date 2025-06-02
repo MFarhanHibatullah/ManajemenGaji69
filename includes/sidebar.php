d8b29d63f8de115125b651e4af30734809f3257b
MFarhanHibatullah <farhanhibatullah07@gmail.com>
2025-06-02 09:28:50 +0700

    first commit

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -7,13 +7,13 @@
 </head>
 <body>
         <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 250px; height: 100vh;">
-            <a href="dashboard.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+            <a href="index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                 <img src="assets/img/logo-bulat.png" alt="Logo" width="60" class="me-2" style="border-radius: 50%;">
                 <span class="fs-6 fw-bold">PT.FARHANTEC INNOVASI</span>
             </a>
             <hr>
             <ul class="nav nav-pills flex-column mb-auto">
-                <li><a href="dashboard.php" class="nav-link text-white">Dashboard</a></li>
+                <li><a href="index.php" class="nav-link text-white">Dashboard</a></li>
                 <li><a href="karyawan.php" class="nav-link text-white">Daftar Karyawan</a></li>
                 <li><a href="jabatan.php" class="nav-link text-white">Daftar Jabatan</a></li>
                 <li><a href="rating.php" class="nav-link text-white">Daftar Rating</a></li>
